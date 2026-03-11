99fbd847e9e07cc35b68dd6e6465ffb7a0c0b43e
Alissa Andrea Storm <140435292+A-St0rM@users.noreply.github.com>
2026-03-11 04:20:51 +0000

    Finished

--- Queue.h
+++ Queue.h
@@ -12,38 +12,38 @@ public:
     ~Queue();
 };
 
-
 void Queue::enqueue(int x){
-  NodePtr new_node= new NODE(x);
-if(new_node){ 
-    if(size>0){
-        tailPtr->set_next(new_node);
+    NodePtr new_node = new NODE(x);
+    if(new_node){ 
+        if(size > 0){
+            tailPtr->set_next(new_node);
+        }
+        else{
+            headPtr = new_node;
+        }
+        tailPtr = new_node;
+        size++;
     }
-    else{
-        headPtr = new_node;
-    }
-    tailPtr=new_node; size++;
-    /* Add head and tail for me please
-   1. connect & Change tail
-  2. (may be) change head  when the queue is empty
-  3. increase size */
-	
- }
 }
 
 int Queue::dequeue(){
-  if(size>0){
-    NodePtr t = headPtr;
-    headPtr=headPtr->get_next();
-    size--;
-    delete t;
-     /* Add head and tail for me please */
+    if(size > 0){
+        NodePtr t = headPtr;
+        int x = t->get_value(); 
+        headPtr = headPtr->get_next();
+        size--;
 
-  }
-  cout<<"Empty Queue";
-  return -1;
+        if(size == 0){
+            tailPtr = 0;
+        }
+
+        delete t;
+        return x;
+    }
+
+    cout << "Empty Queue";
+    return -1;
 }
-
 
 Queue::Queue(){
     size = 0;
